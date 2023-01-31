a98ad190caa4d5727a74e5d9ff84041e71dfae22
Anton Eroshkin <amigos29@yandex.ru>
2023-01-31 18:21:35 +0300

    initial commit

---
+++ HW_request_ex.2.py
@@ -0,0 +1,36 @@
+import requests
+from pprint import pprint
+
+TOKEN = ' '
+
+
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net:443/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        # pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+# if __main__ == '__main__'
+ya = YandexDisk(token=TOKEN)
+print(ya.upload_file_to_disk(disk_file_path='Python/list_chars.txt', filename='list_chars.txt'))
